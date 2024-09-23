9865c9cb5fe4e4a5cb13ac6f06f433195af23113
Alexander Volobuev <fanatov@gmail.com>
2024-09-23 20:10:15 +0300

    second commit

--- DATA.py
+++ DATA.py
@@ -2,11 +2,9 @@ class Urls:
     MAIN_PAGE_URL = f'https://stellarburgers.nomoreparties.site/'
     FORGOT_PASSWORD_URL = f'{MAIN_PAGE_URL}forgot-password'
     RESET_PASSWORD_URL = f'{MAIN_PAGE_URL}reset-password'
-    LOGIN_URL = f'{MAIN_PAGE_URL}login'
-    FORGOT_PASSWORD_URL = f'{MAIN_PAGE_URL}forgot-password'
-    ORDER_URL = f'{MAIN_PAGE_URL}feed'
     ACCOUNT_URL = f'{MAIN_PAGE_URL}account/profile'
-
+    LOGIN_URL = f'{MAIN_PAGE_URL}login'
+    ORDER_URL = f'{MAIN_PAGE_URL}feed'
 
 
 class TestData:
