84af64710ab5802092965e9abd44ff951fa954ab
Alexander Volobuev <fanatov@gmail.com>
2024-09-23 19:11:59 +0300

    first commit

---
+++ DATA.py
@@ -0,0 +1,14 @@
+class Urls:
+    MAIN_PAGE_URL = f'https://stellarburgers.nomoreparties.site/'
+    FORGOT_PASSWORD_URL = f'{MAIN_PAGE_URL}forgot-password'
+    RESET_PASSWORD_URL = f'{MAIN_PAGE_URL}reset-password'
+    LOGIN_URL = f'{MAIN_PAGE_URL}login'
+    FORGOT_PASSWORD_URL = f'{MAIN_PAGE_URL}forgot-password'
+    ORDER_URL = f'{MAIN_PAGE_URL}feed'
+    ACCOUNT_URL = f'{MAIN_PAGE_URL}account/profile'
+
+
+
+class TestData:
+    EMAIL = 'TEST@YAA.RUU'
+    PASSWORD = '12345678'
